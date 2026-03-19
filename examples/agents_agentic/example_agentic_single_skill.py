9cb3b57db75f00ab777cc160e9f56a55f2bcf249
Stelios Sotiriadis <steliosot@msn.com>
2026-03-19 20:42:10 +0000

    updates

--- examples/agents_agentic/example_agentic_single_skill.py
+++ examples/agents_agentic/example_agentic_single_skill.py
@@ -1,8 +1,13 @@
-from comfy_agent import run_agentic
+from comfy_agent import reasoning_agentic, run_agentic
 
+prompt = "cinematic product photo of a bottle of Coca-Cola on a kitchen counter, realistic lighting"
+# prompt = "cinematic product video clip of a bottle of Coca-Cola on a kitchen counter, realistic lighting"
+
+reasoning_agentic(prompt=prompt, print_output=True)
 
 result = run_agentic(
-    prompt="cinematic product photo of a bottle of Coca-Cola on a kitchen counter, realistic lighting",
+    prompt=prompt,
+    print_reasoning=False,
 )
 
 print(result)
